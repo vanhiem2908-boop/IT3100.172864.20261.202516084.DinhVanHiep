21f5314f19f08f32b2603ed94e96c11ab5e079be
Dinh Van Hiep <vanhiem2908@gmail.com>
2026-09-14 12:33:13 +0700

    Update assignment 3.2.1

--- Lab01/HelloWorld.java
+++ Lab01/HelloWorld.java
@@ -1,5 +1,9 @@
+//Example 1: HelloWorld.java
+//Text-printing program
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+    public static void main(String args[]){
+        System.out.println("Xin chao cac ban!");
+        System.out.println("Hello \t world!");
+    } // end of method main
 }
